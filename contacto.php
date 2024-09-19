bc093df8930bb9cf777814f3a95949b2cdbd543f
Jocsan <124210341+Jocuz@users.noreply.github.com>
2024-09-18 23:29:57 -0600

    Update contacto.php

--- contacto.php
+++ contacto.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bruno+Ace+SC&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&display=swap" rel="stylesheet">
     <title>Contacto</title>
     <style>
         body {
@@ -13,18 +17,46 @@
         .container {
             max-width: 600px;
             margin: 0 auto;
-            background: white;
+            background-color: #082032;
             padding: 20px;
             border-radius: 10px;
             box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1);
+        }
+        .btn {
+            background-color: #373739;
+            color: white;
+            padding: 10px 20px;
+            text-decoration: none;
+            border-radius: 5px;
+            display: inline-block;
+            margin-top: 20px;
+        }
+        .btn:hover {
+            background-color: #7a695d;
+        }
+        .bruno-ace-sc-regular {
+            font-family: "Bruno Ace SC", sans-serif;
+            font-weight: 400;
+            font-style: normal;
+            color: white;
+        }
+        .orbitron-uniquifier {
+            font-family: "Orbitron", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: <weight>;
+            font-style: normal;
+            color: white;
         }
     </style>
 </head>
 <body>
     <div class="container">
-        <h1>Contacto</h1>
-        <p>Nombre: Jocsan Uziel Martínez López</p>
-        <img src="" alt="Mi Foto" style="width:200px;height:auto;">
+        <h1 class="orbitron-uniquifier">Contacto</h1>
+        <p class="bruno-ace-sc-regular">Nombre: Jocsan Uziel Martínez López</p>
+        <img src="avatar.png" alt="Mi Foto" style="width:200px;height:auto;">
+        <br><br>
+        <a href="index.php" class="btn">Docker info</a>
     </div>
 </body>
 </html>
+
